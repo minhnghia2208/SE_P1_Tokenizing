68b15e085185f6bf2c2891188c5bef621defa587
minhnghia2208 <nghiapham@umass.edu>
2022-02-04 00:55:27 -0500

    part1 commit

--- tokenizer.py
+++ tokenizer.py
@@ -1,71 +1,74 @@
-# Global variables
+# Connection String
 fStopWord = "stopwords.txt"
 fTokenA = "tokenization-input-part-A.txt"
 fTokenB = "tokenization-input-part-B.txt"
 #
 
 def tokenization():
-    # Input: string
-    # Return: string
-    def abbrivation(str):
-        # Convert string to char array as string isn't mutable
-        arr = list(str)
-
-        i = 0
-        while i < len(arr):
-            if i < len(arr) and arr[i] == '.': 
-                del arr[i]
-            else: i += 1
-                
-        return ''.join(arr)
-    
-    # Input: string
+    # Param: string
     # Return: string
     def contraction(str): 
         return str.replace("'", "")
+    
+    # Param: string
+    # Return: string[]
+    def abbrivation(str):       
+        arr = str.split('.')
+        for ch in arr:
+            if len(ch) > 1: return punctuation(str)
         
-    # Input: string
+        return [''.join(arr)]
+    
+    # Param: string
     # Return: string[]
     def punctuation(str):
-        # Convert string to char array as string isn't mutable
+        ans = []
+        isPun = False
+        # string to char array as string isn't mutable
         arr = list(str)
-        res = []
-
-        i = 0
-        while i < len(arr):
-            # check if an alphabectical character
-            if i < len(arr):
-                asc = ord(arr[i])
-                
-                if not (
-                    (asc >= 97 and asc <= 122) or # lower case char
-                    (asc >= 65 and asc <= 90) or # upper case char
-                    (asc >= 48 and asc <= 57)   # number char
-                    ): res.append(str.split(arr[i])[0])
+        
+        for i in range(0, len(arr)):
+            asc = ord(arr[i])
+            if not (
+                (asc >= 97 and asc <= 122) or # lower case char
+                (asc >= 48 and asc <= 57)   # number
+                ):                
+                isPun = True
+                # recursively remove punctuation
+                for j in str.split(arr[i]):
+                    temp = punctuation(j)
                     
-            i += 1
-            
-        return res
-    
-    # Input: string
-    # Return: string
-    def lowerCase(str):
-        print('test')
-    
-    # f = open(fTokenA, "r")
-    # line = f.readline()
-    # while line:
-    #     arr = line.split()
+                    # Concat new tokenized word to ans array
+                    for k in temp:
+                        ans.append(k)
+                
+                # stop at the first punctuation 
+                break
         
-    #     for ele in arr:
-    #         res = abbrivation(ele)
-    #     line = f.readline()
+        if isPun: return ans
+        else: return [str]
+    
+    # Body
+    f = open(fTokenA, "r")
+    ans = []
+    line = f.readline()
+    while line:
+        arr = line.split()
+                
+        for ele in arr:
+            ele = contraction(ele.lower())
+            for k in abbrivation(ele):
+                ans.append(k)
+            # ele = punctuation(ele)
+        line = f.readline()
+    
+    print(ans)
         
-    # f.close()
+    f.close()
     
     # UNIT TEST AREA
-    # print(abbrivation("U.S.A.."))
+    # print(abbrivation("u.s.d.a.s.d"))
     # print(contraction("don't"))
-    print(punctuation("200,000,000,"))
+    # print(punctuation("100,200*300"))
     
 tokenization()
